c567130a7d6f108267f4028e9eb4f9c3d00fafbe
duyclad <coleanhduy.study@gmail.com>
2021-01-04 02:50:50 +0700

    202101040250

---
+++ DOANCuoiKyNET/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using DOANCuoiKyNET.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOANCuoiKyNET.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        
+
+
+        private readonly MyDBContext _context;
+
+        public UsersController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            
+
+            return View(_context.Users);
+        }
+
+        [HttpPost]
+        public IActionResult TimAjax(string keyword)
+        {
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                var dsLoai = from p in _context.LoaiSPs
+                             join c in _context.Users on p.idUser equals c.idUser
+                             where p.tenLoaiSP.Contains(keyword) || p.metaTitle.Contains(keyword)
+                             select new User_Loai
+                             {
+                                 idLoaiSP = p.idLoaiSP,
+                                 hinhAnh = p.hinhAnh,
+                                 tenLoaiSP = p.tenLoaiSP,
+
+                                 metaTitle = p.metaTitle,
+                                 trangThai = p.trangThai,
+                                 ngayTao = p.ngayTao,
+                                 ngayCapNhat = p.ngayCapNhat,
+                                 hoUser = c.hoUser,
+                                 tenUser = c.tenUser
+                             };
+                return PartialView(dsLoai);
+
+            }
+
+            var dsLoai2 = from p in _context.LoaiSPs
+                          join c in _context.Users on p.idUser equals c.idUser
+                          // where p.tenLoaiSP.Contains(keyword) || p.metaTitle.Contains(keyword)
+                          select new User_Loai
+                          {
+                              idLoaiSP = p.idLoaiSP,
+                              hinhAnh = p.hinhAnh,
+                              tenLoaiSP = p.tenLoaiSP,
+
+                              metaTitle = p.metaTitle,
+                              trangThai = p.trangThai,
+                              ngayTao = p.ngayTao,
+                              ngayCapNhat = p.ngayCapNhat,
+                              hoUser = c.hoUser,
+                              tenUser = c.tenUser
+                          };
+
+
+
+
+
+
+            return PartialView(dsLoai2);
+        }
+
+
+
+
+    }
+}
